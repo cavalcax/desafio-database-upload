a10d7c5f5ff2dcd08e62cb3caec76c3c088a9b10
Augusto Cavalca <guto.cavalca@gmail.com>
2020-06-17 10:25:48 -0300

    chore: Implementação do desafio database upload

--- src/services/DeleteTransactionService.ts
+++ src/services/DeleteTransactionService.ts
@@ -1,8 +1,24 @@
-// import AppError from '../errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import AppError from '../errors/AppError';
+import TransactionsRepository from '../repositories/TransactionsRepository';
 
 class DeleteTransactionService {
-  public async execute(): Promise<void> {
-    // TODO
+  public async execute(id: string): Promise<void> {
+    const transactionsRepository = getCustomRepository(TransactionsRepository);
+
+    const transaction = await transactionsRepository.findOne(id);
+
+    if (!transaction) {
+      throw new AppError('Transaction does not exist!');
+    }
+
+    const { total } = await transactionsRepository.getBalance();
+
+    if (transaction.type === 'income' && total < transaction.value) {
+      throw new AppError('Your balance will be negative.');
+    }
+
+    await transactionsRepository.remove(transaction);
   }
 }
 
